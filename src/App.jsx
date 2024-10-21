60142d83877c4abf71787852d26971d90a9cc06f
Amy <yambeddm@gmail.com>
2024-10-21 15:44:29 +0200

    Header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Header from './components/Header'
 import PhotoCard from './components/PhotoCard'
 
 function App() {
@@ -6,10 +7,13 @@ function App() {
   console.log(apiKey)
 
   return (
+    <>
+    <Header />
     <PhotoCard
     url='https://media1.tenor.com/m/B8rKEjDfDjwAAAAd/cat-clown.gif'
     title='clown_cat'
     />
+    </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { AppBar, Toolbar, Box, Typography, Link } from '@mui/material';
+
+function Header(){
+    return(
+        <AppBar position='fixed'>
+            <Toolbar>
+            <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+          <Link href="/" color="inherit" underline="none">
+            <Typography variant="h6">
+              Nasa APOD App
+            </Typography>
+          </Link>
+        </Box>
+            </Toolbar>
+        </AppBar>
+
+    )
+}
+
+export default Header;
