349c3a033a375f3f27245b633a8c7d0567fde710
Amy <yambeddm@gmail.com>
2024-10-25 00:39:25 +0200

    added infinite scroll, overflow bug

--- src/components/GalleryPage.jsx
+++ src/components/GalleryPage.jsx
@@ -4,18 +4,24 @@ import PhotoList from "./PhotoList";
 import LoadingSpinner from "./LoadingSpinner";
 import { Box } from "@mui/material";
 
-function GalleryPage({ setPhotos }) {
+function GalleryPage() {
   const apiKey = import.meta.env.VITE_API_KEY;
-  const [photos, setLocalPhotos] = useState([]);
+  const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const loadMorePhotos = async () => {
+    const response = await axios.get(
+      `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=20`
+    );
+    setPhotos((prevPhotos) => [...prevPhotos, ...response.data]);
+  };
   useEffect(() => {
     const fetchPhotos = async () => {
       const response = await axios.get(
-        `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=12`
+        `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=20`
       );
       console.log(response.data);
-      setLocalPhotos(response.data);
+      setPhotos(response.data);
       setLoading(false);
     };
     fetchPhotos();
@@ -29,12 +35,13 @@ function GalleryPage({ setPhotos }) {
             justifyContent: "center",
             alignItems: "center",
             height: "100vh",
+            overflowY: "scroll",
           }}
         >
           <LoadingSpinner />
         </Box>
       ) : (
-        <PhotoList photos={photos} />
+        <PhotoList photos={photos} loadMore={loadMorePhotos} />
       )}
     </>
   );
--- src/components/PhotoList.jsx
+++ src/components/PhotoList.jsx
@@ -1,10 +1,14 @@
 import { ImageList, ImageListItem } from "@mui/material";
 import ModalPhoto from "./ModalPhoto";
 import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import LoadingSpinner from "./LoadingSpinner";
 
-function PhotoList({ photos }) {
+function PhotoList({ photos, loadMore }) {
   const [open, setOpen] = useState(false);
   const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [items, setItems] = useState(photos.slice(0, 20));
+  const [hasMore, setHasMore] = useState(true);
 
   const handleOpen = (photoData) => {
     setSelectedPhoto(photoData);
@@ -19,18 +23,31 @@ function PhotoList({ photos }) {
 
   return (
     <>
-      <ImageList cols={6}>
-        {photos.map((photo) => (
-          <ImageListItem key={photo.url}>
-            <img
-              src={`${photo.url}`}
-              alt={photo.title}
-              onClick={() => handleOpen(photo)}
-              style={{ cursor: "pointer" }}
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
+      <InfiniteScroll
+        dataLength={photos.length}
+        next={loadMore}
+        hasMore={true}
+        loader={<LoadingSpinner />}
+      >
+        <ImageList
+          cols={6}
+          sx={{
+            overflow: "hidden", // Empêche PhotoList d'avoir une barre de défilement
+            height: "auto", // Permet d'ajuster la taille automatiquement
+          }}
+        >
+          {photos.map((photo) => (
+            <ImageListItem key={photo.url}>
+              <img
+                src={`${photo.url}`}
+                alt={photo.title}
+                onClick={() => handleOpen(photo)}
+                style={{ cursor: "pointer" }}
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </InfiniteScroll>
       {selectedPhoto && (
         <ModalPhoto
           open={open}
